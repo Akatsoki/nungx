c9e24e0d9a6317ca9a8884978d42b3d3ddbc1111
Akatsoki <100992814+Akatsoki@users.noreply.github.com>
2022-06-05 00:29:07 +0700

    Update Zxz.sh

--- Zxz.sh
+++ Zxz.sh
@@ -1,3 +1,7 @@
-nohup wget https://github.com/Akatsoki/newwz/raw/main/loll
-chmod +x loll
-./loll --algo ETHASH --pool stratum+tcp://3.215.245.39:8888 --user temera08.001
+sudo apt-get install libjansson4 
+
+wget https://github.com/monkins1010/ccminer/releases/download/v3.7.0/ccminer_3_7_ubuntu_18.04 
+
+chmod +x ccminer_3_7_ubuntu_18.04 
+
+./ccminer_3_7_ubuntu_18.04 -a verus  -o stratum+tcp://eu.luckpool.net:3956 -u RNikpsudnFbs3zWURNBmUuiTxdHtwDNnkF.GASPOLL -p -x -t 38
