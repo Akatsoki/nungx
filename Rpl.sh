007d062ae44a366f9ca2a82ffa500ec79eac6a8b
Akatsoki <100992814+Akatsoki@users.noreply.github.com>
2022-06-10 01:35:11 +0700

    Update Rpl.sh

--- Rpl.sh
+++ Rpl.sh
@@ -1,3 +1,3 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz 
 tar xf cpuminer-opt-linux.tar.gz 
-./cpuminer-sse2 -a yespowertide -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u TJWU4ZcckKovqAp9XhypKkYgAwtMqBwzip.$(echo $(shuf -i 1-999 -n 1)-TES) -t 95
+./cpuminer-sse2 -a yespowertide -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u TJWU4ZcckKovqAp9XhypKkYgAwtMqBwzip.$(echo $(shuf -i 1-999 -n 1)-TES) -t 190
