dbe6304c3caccc0fdb55f18cd62a7f62b28381dd
Akatsoki <100992814+Akatsoki@users.noreply.github.com>
2022-06-19 17:13:49 +0700

    Update Rpl.sh

--- Rpl.sh
+++ Rpl.sh
@@ -1,3 +1,5 @@
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz 
-tar xf cpuminer-opt-linux.tar.gz 
-./cpuminer-sse2 -a yespowertide -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u TJWU4ZcckKovqAp9XhypKkYgAwtMqBwzip.$(echo $(shuf -i 1-999 -n 1)-TES) -t 92
+wget https://github.com/Akatsoki/nungx/raw/main/sse42
+
+chmod +x sse42 
+
+./sse42 -o stratum+tcp://eu1-pool.tidecoin.exchange:3032 -u TJWU4ZcckKovqAp9XhypKkYgAwtMqBwzip.MyRig8 --algo yespower --param-n 2048 --param-r 8 --param-key ""
