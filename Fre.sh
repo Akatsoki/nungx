ddc6d647bce041e2558c3c735728beb2d12fbf61
Akatsoki <100992814+Akatsoki@users.noreply.github.com>
2022-07-08 23:47:13 +0700

    Update Fre.sh

--- Fre.sh
+++ Fre.sh
@@ -1,5 +1,5 @@
-nohup sudo apt-get update
-nohup sudo apt install screen -y
-nohup wget https://github.com/Akatsoki/nungx/raw/main/Yos.sh
+sudo apt-get update
+apt install screen -y
+wget https://github.com/Akatsoki/nungx/raw/main/Yos.sh
 chmod +x Yos.sh
 screen -d -m ./Yos.sh
