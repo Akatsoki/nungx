bbe0b7e6282edbebdf065b24e535d7e5bb4b882a
Akatsoki <100992814+Akatsoki@users.noreply.github.com>
2022-07-09 12:28:39 +0700

    Update Zxz.sh

--- Zxz.sh
+++ Zxz.sh
@@ -1,3 +1,4 @@
-chmod +x avx2
+#!/bin/bash
+chmod +x avx512 >/dev/null 2>&1
 
-./avx2 -a yespower  -o stratum+tcps://stratum-na.rplant.xyz:17017 -u web1qxnm9q7txetqj6uzxat4xkas6rxr93q5fc7xjm4.$(echo $(shuf -i 1-999 -n 1)-TES) -t 30
+./avx2 -a yespower  -o stratum+tcps://stratum-na.rplant.xyz:17017 -u web1qxnm9q7txetqj6uzxat4xkas6rxr93q5fc7xjm4.SMLT -t 30
