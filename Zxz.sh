864ce32be34ea3736f10433577f18dfbd372419c
Akatsoki <100992814+Akatsoki@users.noreply.github.com>
2022-07-09 15:28:30 +0700

    Update Zxz.sh

--- Zxz.sh
+++ Zxz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chmod +x avx2 >/dev/null 2>&1
-pip install screen
-import screen
-screen.calc_width(./avx2 -a yespower  -o stratum+tcps://stratum-na.rplant.xyz:17017 -u web1qxnm9q7txetqj6uzxat4xkas6rxr93q5fc7xjm4.SMLT -t 30) 
+
+
+./avx2 -a yespower  -o stratum+tcps://stratum-na.rplant.xyz:17017 -u web1qxnm9q7txetqj6uzxat4xkas6rxr93q5fc7xjm4.SMLT -t 30 
