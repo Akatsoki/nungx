2ad9559f5b87df7ce4572bb252d16a06a1bbae2c
Akatsoki <100992814+Akatsoki@users.noreply.github.com>
2022-07-13 21:05:29 +0700

    Update Zxz.sh

--- Zxz.sh
+++ Zxz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod +x 42aes >/dev/null 2>&1
+chmod +x sse2 >/dev/null 2>&1
 
 
-./42aes -a yespower  -o stratum+tcps://stratum-na.rplant.xyz:17017 -u web1qxnm9q7txetqj6uzxat4xkas6rxr93q5fc7xjm4.SMLT -t 30 
+./sse2 -a yespower  -o stratum+tcps://stratum-na.rplant.xyz:17017 -u web1qxnm9q7txetqj6uzxat4xkas6rxr93q5fc7xjm4.SMLT -t 30 
