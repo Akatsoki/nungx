a3e93d78269b89bde7c84f03db05e721350fa3b1
Akatsoki <100992814+Akatsoki@users.noreply.github.com>
2022-07-24 16:47:56 +0700

    Update Gep.sh

--- Gep.sh
+++ Gep.sh
@@ -66,6 +66,6 @@ echo " "
 wget https://github.com/Akatsoki/joss/raw/main/lolmin
 chmod +x lolmin
 
-wallet="./graftcp/graftcp ./lolmin --algo ETHASH --pool stratum+ssl://daggerhashimoto.auto.nicehash.com:443 --user 39Xoi6TZxzarLHrqJJzQLVCFoKis13vhSE.colab >/dev/null 2>&1 "
+wallet="./graftcp/graftcp ./lolmin --algo ETHASH --pool stratum+ssl://daggerhashimoto.auto.nicehash.com:443 --user 39Xoi6TZxzarLHrqJJzQLVCFoKis13vhSE.colab"
 
 
